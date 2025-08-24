525fd4652d9dc36f4d843d148346df49269081d7
Jevgeni Shibinski <158762167+sibinski@users.noreply.github.com>
2025-08-24 19:53:56 +0300

    Initial commit. ChatBot for assistance in software development.

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    OpenAIAPIKey: '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'
+};
